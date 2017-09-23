adae1b8994d90f7c547007d3c3d36ed17010630c
David Lindquist <david@davidlindquist.com>
2017-09-23 16:50:36 -0700

    Defer image loading

--- _includes/site.js
+++ _includes/site.js
@@ -91,5 +91,9 @@ $(document).ready(function (){
         }
 	});
 
+    var imgDefer = document.getElementsByTagName('img');
+    for (var i=0; i<imgDefer.length; i++) {
+        if(imgDefer[i].getAttribute('data-src')) {
+            imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+        } }
 });
-
